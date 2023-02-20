298dc5c4f13b0fb8d31599a330f11d1849b8aa6e
M-Phoenixx <mchuhan66@gmail.com>
2023-02-20 23:25:21 +0530

    basic structure done

---
+++ src/comp/Card.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./card.css";
+
+const Card = () => {
+  return (
+    <div className="card_container">
+      <img
+        src="https://static.wixstatic.com/media/ea6ac8_b6b0cbe25615488e855f515846354dda~mv2.jpg/v1/fill/w_640,h_420,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/ea6ac8_b6b0cbe25615488e855f515846354dda~mv2.jpg"
+        alt="net slow hai tera"
+      />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores,
+        quisquam obcaecati dolorum dolore quaerat voluptas natus sed nisi
+        nostrum hic dolor reprehenderit sit, pariatur vel voluptatem repellat,
+        eum asperiores? Autem? Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Neque blanditiis porro, consequatur ad magnam debitis
+        omnis nemo quam commodi in impedit tempore molestiae adipisci sequi.
+        Alias, voluptates. Libero, odio voluptates.
+      </p>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/pages/Home.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Home.css";
+import Card from "../comp/Card";
+
+const Home = () => {
+  return (
+    <div className="home_container">
+      <div className="input">
+        <input type="text" placeholder="Search" />
+      </div>
+      <div className="cards">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
